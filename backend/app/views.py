c491f22fad858f5888ae7019e9361ca30abc460a
KemuelRocha <kemuelsr@gmail.com>
2023-03-03 00:57:05 -0300

    view sign

--- backend/app/views.py
+++ backend/app/views.py
@@ -4,6 +4,28 @@ from rest_framework import status
 from .serializers import VerifierSerializer, SignSerializer
 from icpyedu import signer
 
+def assinar_certificado(file_pdf, password, email, file_certificado) -> bool:
+    lib = signer.Sign()
+    return(lib.signFile(email, password, file_pdf, file_certificado))
+
+class AssinarView(APIView):
+
+    def post(self, request, format=None):
+
+        print(request.data)
+
+        serializer = SignSerializer(request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+        
+        file_pdf = data['file_pdf']
+        file_certificado = data['ac_raiz']
+        email = data['email']
+        password = data['password']
+
+        file_result = assinar_certificado(file_pdf, password, email, file_certificado)
+
+
 
 def verificar_certificado(file_pdf, ac_raiz, ac_pessoa) -> bool:
     lib = signer.Verifier()
@@ -28,11 +50,11 @@ class VerificarView(APIView):
 
         if flag_verificado:
             return Response(
-                {'response': 'Arquivo certificado' },
+                {'response': 'Arquivo Assinado Digitalmente' },
                 status=status.HTTP_200_OK
             ) 
         else:
             return Response(
-                {'response': 'Arquivo não certificado' },
+                {'response': 'Arquivo Não Assinado' },
                 status=status.HTTP_200_OK
             )  
